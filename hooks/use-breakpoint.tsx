93df3611969f75a50dbb38bcf2ba05ad48f2c13e
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2025-03-12 12:11:42 +0000

    getting ride of react-responsive since we're not using it

--- hooks/use-breakpoint.tsx
+++ hooks/use-breakpoint.tsx
@@ -1,18 +1,27 @@
-import { useMediaQuery } from "react-responsive";
 import resolveConfig from "tailwindcss/resolveConfig";
 import tailwindConfig from "../tailwind.config";
+import { useEffect } from "react";
+import React from "react";
 
 const breakpoints = resolveConfig(tailwindConfig).theme.screens;
 
 type BreakpointKey = keyof typeof breakpoints;
 
 export function useBreakpoint<K extends BreakpointKey>(breakpointKey: K) {
-	const bool = useMediaQuery({
-		query: `(min-width: ${breakpoints[breakpointKey]})`,
-	});
+	const [matches, setMatches] = React.useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(`(min-width: ${breakpoints[breakpointKey]})`);
+		const handleChange = () => setMatches(mediaQuery.matches);
+		handleChange(); // Set initial value
+
+		mediaQuery.addEventListener("change", handleChange);
+		return () => mediaQuery.removeEventListener("change", handleChange);
+	}, [breakpointKey]);
+
 	const capitalizedKey = breakpointKey[0].toUpperCase() + breakpointKey.substring(1);
 	type Key = `is${Capitalize<K>}`;
 	return {
-		[`is${capitalizedKey}`]: bool,
+		[`is${capitalizedKey}`]: matches,
 	} as Record<Key, boolean>;
 }
