66c758ab843254eae65f0a29b38e99aee80c9e88
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2025-10-02 13:43:14 +0100

    changing about section text

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -6,34 +6,33 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const { ref } = useSectionInView({ sectionName: "About" });
+	const { ref } = useSectionInView({ sectionName: "About" });
 
-  return (
-    <motion.section
-      className="max-w-[45rem] scroll-mt-28 mb-[8rem] text-center leading-8 sm:mb-40"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
-      id="about"
-      ref={ref}
-    >
-      <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        {`After completing `}
-        <span className="font-semibold">{`1337 school cursus. `}</span>
-        {`I decided to continue pursue real world experience in programming. Eventually, I secured an intership at UM6P as a `}
-        <span className="font-semibold">{`full stack developer using django and nextjs. `}</span>
-        <span className="italic">{`My favourite part of programming `}</span>
-        {` is the problem-solving aspect. I `}
-        <span className="underline">{`love `}</span>
-        {`the feeling of finally figuring out a solution to a problem. I am currently looking for a full-time position as a software developer.`}
-      </p>
-      <p>
-        <span className="italic">{`When `}</span>
-        {`I'm not coding. I enjoy playing video games, watching movies, and playing with my cat. I also enjoy `}
-        <span className="font-semibold">{`learning new things. `}</span>
-        {`I am currently learning about history and economics.`}
-      </p>
-    </motion.section>
-  );
+	return (
+		<motion.section
+			className="max-w-[45rem] scroll-mt-28 mb-[8rem] text-center leading-8 sm:mb-40"
+			initial={{ opacity: 0, y: 100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.175 }}
+			id="about"
+			ref={ref}
+		>
+			<SectionHeading>About me</SectionHeading>
+			<p className="mb-3">
+				<p className="mb-3">
+					{`Hi there, my name is Azeddine Hamdaoui. I am a graduate of `}
+					<span className="font-semibold">{`1337 School, part of the 42 network, `}</span>
+					{`where I learned to build projects from scratch, including mini shell prompts, standard C libraries, and more, gaining a strong foundation in modern software development.`}
+				</p>
+
+				<p className="mb-3">
+					{`Most of my work was in small team projects, which helped me develop collaboration and problem-solving skills. Currently, I am a junior software engineer seeking an internship or entry-level position.`}
+				</p>
+
+				<p>
+					{`I'm passionate about building web applications and enjoy diversifying and deepening my knowledge of technologies, ranging from database design to frontend development.`}
+				</p>
+			</p>
+		</motion.section>
+	);
 }
